15bfddb4041b59e080c69e0c2b15a0922c904fff
fr3kz <tektmap1@gmail.com>
2019-07-13 14:21:05 +0200

    static files fix

--- wykop/urls.py
+++ wykop/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('posts.urls')),
-]+ static(settings.MEDIA_URL, docuemnt_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #ADD MEDIA
